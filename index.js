88e91c0898105968716c8c0ad201233cd7a05f44
NobleMahmud <mahmud.oneis1@gmail.com>
2023-11-26 03:12:03 +0600

    users server added

--- index.js
+++ index.js
@@ -24,8 +24,22 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    const userCollection = client.db("hostelDb").collection("users");
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      // insert email if user does not exist
+      // you can do this in many ways. (1. email unique, 2. upsert, 3. simple checking)
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+ 
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
